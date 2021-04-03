4b58fa6933b9358537834d49f64b6e0f468d44a1
Guilherme Mendes <guilhermerm@MMB025899SPO.local>
2021-04-02 21:04:56 -0300

    remove unused code from ex 03

--- RM87685_EX03.py
+++ RM87685_EX03.py
@@ -1,6 +1,4 @@
 print("\n***** Calculo de Votos *****\n")
-# monday = int(input("Digite a quantidade de votos para segunda-feira: "))
-# weight = int(input("Digite a quantidade de votos para terça-feira: "))
-# weight = int(input("Digite a quantidade de votos para terça-feira: "))
+
 
 
