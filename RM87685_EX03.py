9ad26af3932f890afa74bde524b6c1f56fccc2e2
Guilherme Ramos Mendes <guilherme.rmendes95@gmail.com>
2021-04-02 22:55:03 -0300

    Solving ex 3

--- RM87685_EX03.py
+++ RM87685_EX03.py
@@ -1,5 +1,21 @@
+def getMaxVotedDayValue(dictionary, key):
+    return dictionary[key]
+
 print("\n***** Calculo de Votos *****\n")
+monday = int(input("Digite a quantidade de votos para segunda-feira: "))
+tuesday = int(input("Digite a quantidade de votos para terça-feira: "))
+wednesday = int(input("Digite a quantidade de votos para quarta-feira: "))
+thursday = int(input("Digite a quantidade de votos para quinta-feira: "))
+friday = int(input("Digite a quantidade de votos para sexta-feira: "))
 
+daysVotesDictionary = {
+    "segunda-feira": monday,
+    "terça-feira": tuesday,
+    "quarta-feira": wednesday,
+    "quinta-feira": thursday,
+    "sexta-feira": friday
+}
 
-
+max_voted_day_key = max(daysVotesDictionary, key=daysVotesDictionary.get)
+print("O dia mais votado foi: ", max_voted_day_key, getMaxVotedDayValue(daysVotesDictionary, max_voted_day_key))
 
